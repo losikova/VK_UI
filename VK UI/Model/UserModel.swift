e7220158ca73da14a66c27feed51a7447bb99203
Anastasia Losikova <anast.losikova@gmail.com>
2022-02-14 19:06:30 +0300

    Add core data and rest api

--- VK UI/Model/UserModel.swift
+++ VK UI/Model/UserModel.swift
@@ -12,3 +12,10 @@ struct User {
     var avatar: UIImage
     var photos: [UIImage]
 }
+
+struct PostsJSON: Decodable {
+    var userId: Int
+    var id: Int
+    var title: String
+    var body: String
+}
